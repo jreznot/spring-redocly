39dff714d1e93f9370b43374512892f838d09c7f
Yuriy Artamonov <jreznot@gmail.com>
2023-05-21 21:41:23 +0200

    Demo controller

--- src/main/java/com/example/SpringRedoclyApplication.java
+++ src/main/java/com/example/SpringRedoclyApplication.java
@@ -2,6 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringRedoclyApplication {
@@ -10,4 +14,19 @@ public class SpringRedoclyApplication {
     SpringApplication.run(SpringRedoclyApplication.class, args);
   }
 
+}
+
+@RestController
+class UserController {
+  @GetMapping("/users")
+  List<User> getAll() {
+    return List.of(
+        new User("root"),
+        new User("manager"),
+        new User("worker")
+    );
+  }
+}
+
+record User(String name) {
 }
